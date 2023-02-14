ac56e961ec205af7500053bc45aa30374f469a8a
itsshivama <92708149+itsshivama@users.noreply.github.com>
2023-02-14 11:51:47 +0530

    buying details service update1

---
+++ Dphone_Customer_Service/src/test/java/com/dphone/customer/DphoneCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dphone.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DphoneCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
