9f185bbc6f860a5f42e26efee8c8abf089a20003
Sankalp-cyber <sankalpgupta811@gmail.com>
2023-02-16 23:39:13 +0530

    updating structure

--- Dphone_Customer_Service/src/main/java/com/dphone/customer/dao/CustomerDao.java
+++ Dphone_Customer_Service/src/main/java/com/dphone/customer/dao/CustomerDao.java
@@ -1,10 +1,10 @@
 package com.dphone.customer.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dphone.customer.entity.CustomerEntity;
 
-
+@Repository
 public interface CustomerDao extends JpaRepository<CustomerEntity, Long>{
-
 }
