44a25b83d4b4fc7ab468065228b369846a1aeaa2
itsshivama <92708149+itsshivama@users.noreply.github.com>
2023-02-17 14:31:45 +0530

    Optimized Imports

--- Dphone_Buying_Details/src/main/java/com/dphone/buying/details/controller/BuyingDetailController.java
+++ Dphone_Buying_Details/src/main/java/com/dphone/buying/details/controller/BuyingDetailController.java
@@ -1,19 +1,11 @@
 package com.dphone.buying.details.controller;
 
 import com.dphone.buying.details.bean.BuyingDetailsBean;
+import com.dphone.buying.details.service.BuyingDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.dphone.buying.details.service.BuyingDetailService;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/product/")
 @RestController
