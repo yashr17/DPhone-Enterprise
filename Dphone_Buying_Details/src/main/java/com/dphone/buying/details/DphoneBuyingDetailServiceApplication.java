dc49287c6fb9422d28ea0cc09fd6fa804aff84eb
itsshivama <92708149+itsshivama@users.noreply.github.com>
2023-02-18 17:31:55 +0530

    Everything Updated

---
+++ Dphone_Buying_Details/src/main/java/com/dphone/buying/details/DphoneBuyingDetailServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dphone.buying.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DphoneBuyingDetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DphoneBuyingDetailServiceApplication.class, args);
+		System.out.println("Buying Details Application Started");
+	}
+
+}
