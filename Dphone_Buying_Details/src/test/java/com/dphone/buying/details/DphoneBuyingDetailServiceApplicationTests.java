20d19d3ae9d66ebd3ec10da2d7567f6ba77db020
itsshivama <92708149+itsshivama@users.noreply.github.com>
2023-02-18 17:19:19 +0530

    Hiii Again

---
+++ Dphone_Buying_Details/src/test/java/com/dphone/buying/details/DphoneBuyingDetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dphone.buying.details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DphoneBuyingDetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
